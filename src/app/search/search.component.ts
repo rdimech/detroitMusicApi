83c73e496845fedd963eb56e40426ab77266c85e
JCSweet <sweetjor@gmail.com>
2019-05-08 22:12:08 -0400

    BucketPage(notFuctioning)

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Output, OnInit  } from '@angular/core';
 import { Api } from '../services/api.service'
+
+interface MapArray {
+  name: string;
+  url: string;
+  startTime: string;
+  favorite: boolean;
+}
 
 interface Events {
   name: string;
@@ -9,15 +16,12 @@ interface Events {
       dateTime: string;
     }
   }
-  
 }
-
 interface ApiData {
-  _embedded:  {
+  _embedded: {
     events: Events[];
   }
 }
-
 @Component({
   selector: 'searchCriteria',
   templateUrl: './search.component.html',
@@ -25,36 +29,43 @@ interface ApiData {
   providers: [Api]
 })
 
-export class SearchCriteriaComponent {
+export class SearchCriteriaComponent implements OnInit {
   title = 'Search';
-  list: object[];
+  list: MapArray[];
+  favorites: [];
   errorMessage: string;
-  searchKeyword: string ='';
+  searchKeyword: string = '';
   formattedKeyword: string;
   favorite: boolean;
 
-  constructor(private api: Api) {}
+// @Output() submitted = new EventEmitter<any>();
 
+  constructor(private api: Api) { }
+
+  ngOnInit() {
+    this.api.Favorites.subscribe(list => {
+      this.favorites = list
+    });
+  }
   getAllEvents = () => {
     this.formattedKeyword = this.searchKeyword.replace(/ /g, '+');
     this.api.getEvents(this.formattedKeyword).subscribe((data: ApiData) => {
       // console.log(data);
-       this.list = data._embedded.events.map(event => {
-         return { name: event.name, url: event.url, startTime: event.dates.start.dateTime, favorite: false }
-       });
-       console.log(this.list);
-       this.errorMessage = null;
-       this.searchKeyword='';
-    }, 
-    error => {
-      this.errorMessage = error.message;
-    }
+      this.list = data._embedded.events.map(event => {
+        return { name: event.name, url: event.url, startTime: event.dates.start.dateTime, favorite: false }
+      });
+      console.log(this.list);
+      this.errorMessage = null;
+      this.searchKeyword = '';
+    },
+      error => {
+        this.errorMessage = error.message;
+      }
     );
   };
-  
+
   saveFavorite = (index) => {
     this.list[index].favorite = !this.list[index].favorite;
-    
-
+    this.api.addFavorite([...this.favorites, this.list[index]]);
   };
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable()
 export class Api {
     apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=266&apikey=MLpkpoNrpU9DB47T6zAKab7EA4bcgRqR'
+    private _favorites = new BehaviorSubject<[]>([]);
+    Favorites: Observable<[]> = this._favorites.asObservable(); 
+    get favorites () {
+        return this._favorites.getValue();
+    }
     constructor(private http: HttpClient) {}
-
     getEvents = (formattedKeyword) => this.http.get(this.apiUrl + '&keyword=' + formattedKeyword);
+
+    addFavorite = nextState => this._favorites.next(nextState)
 }
